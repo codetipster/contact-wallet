f78876aad26ca32e75c98a59eb87ef808704dc1b
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-05-26 21:28:00 +0000

    Header component

--- contact-wallet-app/src/components/App.js
+++ contact-wallet-app/src/components/App.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+import ContactCard from './ContactCard';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
+  );
 }
 
 export default App;
