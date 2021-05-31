dab65ebbc40c1cd57f9e76502972eec9bee0bb7a
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-05-31 20:37:01 +0000

    local storage

--- contact-wallet-app/src/components/App.js
+++ contact-wallet-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
@@ -6,11 +6,22 @@ import ContactCard from './ContactCard';
 
 function App() {
   const [contacts, setContacts] = useState([]);
-
+  const LOCAL_STORAGE_KEY = 'contacts';
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div>
